48ee32c29b7511ddba2facd3d1e37e4b0d195e39
sangeethamoorthy2024@gmail.com <sangeethamoorthy2024@gmail.com>
2024-06-18 10:50:56 +0000

    ok

---
+++ Day1.py
@@ -0,0 +1,50 @@
+"""Python is a widely used general-purpose, high level programming language. It was initially
+designed by Guido van Rossum in 1991 and developed by Python Software Foundation. It
+was mainly developed for emphasis on code readability, and its syntax allows programmers
+to express concepts in fewer lines of code.
+Python is a programming language that lets you work quickly and integrate systems more
+efficiently."""
+
+print("hello world")
+x=[0,1,2]
+print(x)
+print(24656354687654+2)
+print(20)
+print(0b10)
+print(0X20)
+
+# To verify the type of any object in Python, use the type() function:
+type(10)
+a=11
+print(type(a))
+
+
+# Float:
+"""Float, or "floating point number" is a number, positive or negative, containing one or more
+decimals.
+Float can also be scientific numbers with an "e" to indicate the power of 10."""
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+"""
+Boolean:
+Objects of Boolean type may have one of two values, True or False"""
+
+type(True)
+print(type(1==1))
+
+
+"""
+String:
+1. Strings in Python are identified as a contiguous set of characters represented in the
+quotation marks. Python allows for either pairs of single or double quotes.
+• 'hello' is the same as "hello".
+• Strings can be output to screen using the print function. For example: print("hello")."""
+
+
+a="sangeetha"
+print(a)
+
