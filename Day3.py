0a51b789ead041daeb417c53f8817c843c60eaf9
sangeethamoorthy2024@gmail.com <sangeethamoorthy2024@gmail.com>
2024-06-19 05:59:56 +0000

    welcome aws

---
+++ Day3.py
@@ -0,0 +1,61 @@
+# Tuple Data Type
+
+'''
+ a tuple is also an ordered collection of Python objects. 
+ The only difference between a tuple and a list is that tuples are immutable
+ i.e.tuples cannot be modified after it is created.
+ It is represented by a tuple class.
+'''
+
+
+"""
+Tuple1 =  () # empty
+print("Initial empty Tuple: ")
+print(Tuple1)
+
+
+Tuple1 = ('Geeks', 'For',34,9.0)
+print("Tuple with the use of String: ")
+print(Tuple1)
+
+
+list1 = [1, 2, 4, 5, 6]
+print("Tuple using List: ")
+print(tuple(list1)) # tuple(list1)
+
+Tuple1 = tuple("Geeks") # a=tuple("a")
+print("Tuple with the use of function: ")
+print(Tuple1)
+
+Tuple1 = (0, 1, 2, 3) ## output ((),())
+Tuple2 = ('python', 'geek')
+Tuple3 = (Tuple1, Tuple2)
+
+print("\nTuple with nested tuples: ")
+print(Tuple3)
+"""
+"""
+tuple1 = tuple([1, 2, 3, 4, 5])
+print("First element of tuple")
+print(tuple1[0])
+print("\nLast element of tuple")
+print(tuple1[-1])
+
+print("\nThird last element of tuple")
+print(tuple1[-3])
+"""
+"""
+print(type(True))
+print(type(False))
+
+print(type(true))
+
+"""
+
+t1=([1,2,3,4])
+
+print(t1[-1])
+print(t1[0])
+
+
+
